57a320b12f78a1a8747802dc3318223c686b3809
piyas talukder <72153323+piyas1234@users.noreply.github.com>
2021-06-10 05:32:17 -0700

    Update Thumbnail.js

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,25 +1,25 @@
-import { ThumbUpIcon } from '@heroicons/react/outline'
+ import { ThumbUpIcon } from '@heroicons/react/outline'
 import Image from 'next/image'
 import React from 'react'
 
-export default function Thumbnail({result}) {
+export default function Thumbnail({ result }) {
     console.log(result)
     return (
-        <div className="group">
-           <Image
-           layout='responsive'
-           src={`https://image.tmdb.org/t/p/w500/${result.backdrop_path}` || `https://image.tmdb.org/t/p/w500/${result.poster_path}`}
-           height="200px"
-           width="300px"
-           />
-           <div className="p-2">
-               <p className="truncate max-w-md">{result.overview}</p>
-               <h1>{result.title || result.original_name}</h1>
-               <p className="flex items-center opacity-0 group-hover:opacity-100">
-                   {result.release_date} {" "}
-                   <ThumbUpIcon width='20px' /> {result.vote_count}
-               </p>
-           </div>
+        <div className="group p-2 cursor-pointer transition duration-200 ease-in transform sm:hover:scale-x-105 hover:z-50">
+            <Image
+                layout='responsive'
+                src={`https://image.tmdb.org/t/p/w500/${result.backdrop_path}` || `https://image.tmdb.org/t/p/w500/${result.poster_path}`}
+                height="200px"
+                width="300px"
+            />
+            <div className="p-2">
+                <p className="truncate max-w-md">{result.overview}</p>
+                <h1>{result.title || result.original_name}</h1>
+                <p className="flex items-center opacity-0 group-hover:opacity-100">
+                    {result.release_date} {" "}
+                    <ThumbUpIcon width='20px' /> {result.vote_count}
+                </p>
+            </div>
         </div>
     )
 }
